519287fe9ebe2ad9f335253de0d9fb40089474b5
Piseth Ing <piseth.ing@allianz.com>
2022-12-09 14:27:40 +0700

    week 2: CRUD brand, Controller Advice

--- src/main/java/com/piseth/java/school/phoneshopenight/service/impl/BrandServiceImpl.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/impl/BrandServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.piseth.java.school.phoneshopenight.entity.Brand;
+import com.piseth.java.school.phoneshopenight.exception.ResourceNotFoundException;
 import com.piseth.java.school.phoneshopenight.repository.BrandRepository;
 import com.piseth.java.school.phoneshopenight.service.BrandService;
 
@@ -14,6 +15,19 @@ public class BrandServiceImpl implements BrandService{
 
 	@Override
 	public Brand create(Brand brand) {
+		return brandRepository.save(brand);
+	}
+
+	@Override
+	public Brand getById(Integer id) {
+		return brandRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Brand", id));
+	}
+
+	@Override
+	public Brand update(Integer id, Brand brandUpdate) {
+		Brand brand = getById(id);
+		brand.setName(brandUpdate.getName()); // @TODO improve update
 		return brandRepository.save(brand);
 	}
 
